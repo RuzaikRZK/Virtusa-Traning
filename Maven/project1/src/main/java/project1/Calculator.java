84b5f5608dd50324a06baec0d5ade1f263bb8e15
rzktechnology <ruzaikmohomad@gmail.com>
2020-01-22 22:08:51 +0530

    Adding new Project Maven Dependencies

---
+++ Maven/project1/src/main/java/project1/Calculator.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class Calculator {
+
+	public int Multyply(int number1, int number2) {
+
+		int total = number1 * number2;
+		return total;
+
+	}
+
+}
---
+++ Maven/project2/src/main/java/project2/TestCal.java
@@ -0,0 +1,17 @@
+package project2;
+
+import project1.Calculator;
+
+public class TestCal {
+	
+	public static void main(String [] args) {
+		
+			Calculator c = new Calculator();
+			c.Multyply(20, 20);
+			System.out.print(c.Multyply(20, 20));
+		
+		
+	}
+	
+
+}
