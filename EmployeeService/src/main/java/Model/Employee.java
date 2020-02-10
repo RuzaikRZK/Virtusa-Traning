55b2f402dcc6acb220909d0680c564a5bf072e8b
rzktechnology <ruzaikmohomad@gmail.com>
2020-02-10 21:39:37 +0530

    Adding Spring boot REST Full Project with URL and Path Parameters

--- EmployeeService/src/main/java/Model/Employee.java
+++ EmployeeService/src/main/java/Model/Employee.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Employee {
 
 	private String name;
-	private String age;
+	private int age;
 
 	public String getName() {
 		return name;
@@ -16,24 +16,30 @@ public class Employee {
 		this.name = name;
 	}
 
-	public String getAge() {
+	public int getAge() {
 		return age;
 	}
 
-	public void setAge(String age) {
+	public void setAge(int age) {
 		this.age = age;
 	}
 
 	public static List<Employee> getAllEmployee() {
 
-		Employee e = new Employee();
+		Employee e = new Employee("ruzaik",23);
 		List<Employee> employee = new ArrayList<>();
-		e.setName("ruzaik");
-		e.setAge("22");
+		//e.setName("ruzaik");
+		//e.setAge(22);
 		employee.add(e);
 
 		return employee;
 
+	}
+
+	public Employee(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
 	}
 
 }
